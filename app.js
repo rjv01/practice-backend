68fcc26dcd207d2bf10e68482c0df3f7d997050e
rjv01 <verma.rajshekhar1@gmail.com>
2026-01-15 11:47:08 +0530

    adding sec true and port 465:

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(express.json());
 
 
 const transporter = nodemailer.createTransport({
-    secure:false,
+    secure:true,
     host:"smtp.gmail.com",
-    port:587,
+    port:465,
     auth:{
         user:process.env.EMAIL,
         pass:process.env.PASSWORD,
