5b84bb868bf4c0bd4ee10e166d5893aaa4a911c8
rjv01 <verma.rajshekhar1@gmail.com>
2026-01-15 13:17:52 +0530

    using resend instead of node mailer

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import  nodemailer  from "nodemailer";
 import  cors  from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import { Resend } from "resend";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -23,28 +25,36 @@ app.use(
 app.use(express.json());
 
 
-const transporter = nodemailer.createTransport({
-    secure:false,
-    host:"smtp.ethereal.email",
-    // host:"smtp.gmail.com",
-    port:587,
-    // port:465,
-    auth:{
-        user:process.env.EMAIL,
-        pass:process.env.PASSWORD,
-    }
-});
+// const transporter = nodemailer.createTransport({
+//     secure:false,
+//     host:"smtp.ethereal.email",
+//     // host:"smtp.gmail.com",
+//     port:587,
+//     // port:465,
+//     auth:{
+//         user:process.env.EMAIL,
+//         pass:process.env.PASSWORD,
+//     }
+// });
 
-async function sendMail(to,sub,msg){
-    const info = await transporter.sendMail({
-        from:process.env.EMAIL,
+// async function sendMail(to,sub,msg){
+//     const info = await transporter.sendMail({
+//         from:process.env.EMAIL,
+//         to,
+//         subject:sub,
+//         html:msg,
+//     })
+//     console.log("Message sent",info.messageId);
+// };
+
+async function sendMail(to,subject,html) {
+    return await resend.emails.send({
+        from:"onboarding@resend.dev",
         to,
-        subject:sub,
-        html:msg,
-    })
-    console.log("Message sent",info.messageId);
-};
-
+        subject,
+        html
+    });
+}
 
 app.post("/sendyouremailid",async (req,res) => {
     console.log(req.body);
@@ -55,7 +65,7 @@ app.post("/sendyouremailid",async (req,res) => {
     }
     
     try {
-        await sendMail(email,"This is Test subject😐","<h1>This is test message👁️👄👁️<h1>");
+        await sendMail(email,"This is Test subject😐",`<p>Congrats on your joining ${email}<strong>Infosys teams Welcomes you</strong>!</p>`);
         return res.status(200).json({message:"Email sent successfully"});
     } catch (error) {
         console.log(error);
